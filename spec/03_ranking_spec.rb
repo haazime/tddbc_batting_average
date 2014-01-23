e6b20f074144feffc6821c941069ac8de88ad190
haazime <h4zime@gmail.com>
2014-01-23 17:41:35 +0900

    refactor

--- spec/03_ranking_spec.rb
+++ spec/03_ranking_spec.rb
@@ -8,26 +8,38 @@ describe "打率ランキング" do
   context "選手のリストが空" do
     let(:players) { [] }
 
-    it { expect(subject).to be_empty }
+    it { expect(subject).to be_nil }
   end
+
+  let(:a) { Player.new("A") }
+  let(:b) { Player.new("B") }
+  let(:c) { Player.new("C") }
 
   context "playerA=10/8/3, playerB=10/8/4, playerC=10/8/2" do
     let(:players) { [a, b, c] }
-    let(:a) { Player.new("A").score_batting(appearance: 10, bat: 8, hit: 3) }
-    let(:b) { Player.new("B").score_batting(appearance: 10, bat: 8, hit: 4) }
-    let(:c) { Player.new("C").score_batting(appearance: 10, bat: 8, hit: 2) }
 
-    it do
+    before do
+      a.batting_score = { appearance: 10, bat: 8, hit: 3 }
+      b.batting_score = { appearance: 10, bat: 8, hit: 4 }
+      c.batting_score = { appearance: 10, bat: 8, hit: 2 }
+    end
+
+    it "1位B 2位A 3位C" do
       expect(subject.each_with_rank.to_a).to eq([[b, 1], [a, 2], [c, 3]])
     end
   end
 
   context "playerA=10/0/0, playerB=0/0/0, playerC=10/8/2" do
     let(:players) { [a, b, c] }
-    let(:a) { Player.new("A").score_batting(appearance: 10, bat: 0, hit: 0) }
-    let(:b) { Player.new("B").score_batting(appearance: 0, bat: 0, hit: 0) }
-    let(:c) { Player.new("C").score_batting(appearance: 10, bat: 8, hit: 2) }
 
-    it { expect(subject.each_with_rank.to_a).to eq([[c, 1], [a, 2], [b, 3]]) }
+    before do
+      a.batting_score = { appearance: 10, bat: 0, hit: 0 }
+      b.batting_score = { appearance: 0, bat: 0, hit: 0 }
+      c.batting_score = { appearance: 10, bat: 8, hit: 2 }
+    end
+
+    it "1位C 2位A 3位B" do
+      expect(subject.each_with_rank.to_a).to eq([[c, 1], [a, 2], [b, 3]])
+    end
   end
 end
